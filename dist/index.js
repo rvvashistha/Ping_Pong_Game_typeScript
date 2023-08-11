29bf69105afcfe5b2be7e69bfbfba6bfa1f66946
rvvashistha <rohitvashistha171@gmail.com>
2023-08-11 19:21:30 +0530

    Third commit

--- dist/index.js
+++ dist/index.js
@@ -8,8 +8,9 @@ var paddel2 = document.querySelector(".paddle_2");
 var score1 = document.querySelector(".player_1_score");
 var score2 = document.querySelector(".player_2_score");
 var message = document.querySelector(".message");
-var ballcoord = ball.getBoundingClientRect();
-var intialBallCoord = ballcoord;
+var boardCoord = board.getBoundingClientRect();
+var ballCoord = ball.getBoundingClientRect();
+var intialBallCoord = ballCoord;
 var paddle1Coord = paddel1.getBoundingClientRect();
 var paddle2Coord = paddel2.getBoundingClientRect();
 var paddleHeight = paddle1Coord.height;
@@ -25,7 +26,6 @@ var Game = /** @class */ (function () {
         };
     }
     Game.prototype.start = function () {
-        this.state = state.STARTED;
         console.log("Game on!!", this.state);
         this.bindListeners();
     };
@@ -44,12 +44,22 @@ var Game = /** @class */ (function () {
     };
     Game.prototype.getVelocity = function () {
         return {
-            dx: random(3, 8),
+            dx: random(3, 8) * (random(0, 1) ? 1 : -1),
             dy: random(3, 8)
         };
     };
     Game.prototype.moveBall = function (velocity) {
-        console.log(velocity);
+        var _this = this;
+        if (ballCoord.top <= boardCoord.top ||
+            ballCoord.bottom >= boardCoord.bottom) {
+            velocity.dy = -velocity.dy;
+        }
+        ball.style.top = ball.style.top + velocity.dy + "px";
+        ball.style.left = ball.style.left + velocity.dy + "px";
+        ballCoord = ball.getBoundingClientRect();
+        requestAnimationFrame(function () {
+            _this.moveBall(velocity);
+        });
     };
     return Game;
 }());
--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,84 @@
 import { state } from "./utils"
-import { Scores,Velocity,random } from "./utils";
+import { Scores, Velocity, random } from "./utils";
 //DOM Nodes :-
-const board=document.querySelector(".board") as HTMLElement; 
-const ball=document.querySelector(".ball") as HTMLElement; 
-const paddel1=document.querySelector(".paddle_1") as HTMLElement; 
-const paddel2=document.querySelector(".paddle_2") as HTMLElement; 
-const score1=document.querySelector(".player_1_score") as HTMLElement; 
-const score2=document.querySelector(".player_2_score") as HTMLElement; 
-const message=document.querySelector(".message") as HTMLElement; 
+const board = document.querySelector(".board") as HTMLElement;
+const ball = document.querySelector(".ball") as HTMLElement;
+const paddel1 = document.querySelector(".paddle_1") as HTMLElement;
+const paddel2 = document.querySelector(".paddle_2") as HTMLElement;
+const score1 = document.querySelector(".player_1_score") as HTMLElement;
+const score2 = document.querySelector(".player_2_score") as HTMLElement;
+const message = document.querySelector(".message") as HTMLElement;
 
 
-let ballcoord= ball.getBoundingClientRect();
 
-const intialBallCoord= ballcoord;
+let boardCoord=board.getBoundingClientRect();
 
-let paddle1Coord=paddel1.getBoundingClientRect();
-let paddle2Coord=paddel2.getBoundingClientRect();
+let ballCoord = ball.getBoundingClientRect();
 
-let paddleHeight=paddle1Coord.height;
+const intialBallCoord = ballCoord;
 
-let BallTop=ball.style.top;
-let BallLeft=ball.style.left;
+let paddle1Coord = paddel1.getBoundingClientRect();
+let paddle2Coord = paddel2.getBoundingClientRect();
+
+let paddleHeight = paddle1Coord.height;
+
+let BallTop = ball.style.top;
+let BallLeft = ball.style.left;
 
 class Game {
-    private readonly SPEED=0.085; 
+    private readonly SPEED = 0.085;
     private state = state.STOPPED;
-    private scores ={
-        player1:0,
-        player2:0 
+    private scores = {
+        player1: 0,
+        player2: 0
     } as Scores;
 
 
     start() {
-        this.state = state.STARTED;
         console.log("Game on!!", this.state)
         this.bindListeners();
     }
 
-    bindListeners(){
-        document.addEventListener('keydown',(event)=>{
-        if(event.key==="Enter"){
-            this.state=state.STARTED;
-            message.innerHTML="Game on!!"
+    bindListeners() {
+        document.addEventListener('keydown', (event) => {
+            if (event.key === "Enter") {
+                this.state = state.STARTED;
+                message.innerHTML = "Game on!!"
 
-            requestAnimationFrame(()=>{
-                let velocity=this.getVelocity();
-                this.moveBall(velocity);
-            })
-        }
-       
+                requestAnimationFrame(() => {
+                    let velocity = this.getVelocity();
+                    this.moveBall(velocity);
+                })
+            }
+
         })
     }
 
-    getVelocity(){
-       return{
-        dx:random(3,8),
-        dy:random(3,8)
-       } as Velocity
+    getVelocity() {
+        return {
+            dx: random(3, 8) * (random(0, 1) ? 1 : -1),
+            dy: random(3, 8)
+        } as Velocity
     }
 
-    moveBall(velocity:Velocity){
-        console.log(velocity)
+    moveBall(velocity: Velocity) {
+        
+        if(
+            ballCoord.top<=boardCoord.top  ||
+            ballCoord.bottom>= boardCoord.bottom
+        ){
+            velocity.dy=-velocity.dy;
+
+        }
+
+        ball.style.top=ball.style.top + velocity.dy +"px"
+        ball.style.left=ball.style.left + velocity.dy +"px"
+
+        ballCoord=ball.getBoundingClientRect();
+
+        requestAnimationFrame(()=>{
+            this.moveBall(velocity);
+        })
     }
 
 
